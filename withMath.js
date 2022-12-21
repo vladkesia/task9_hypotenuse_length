7df28a2f2719149112900a6b86fd4f3e6fa84f4a
Kesya <kesyavlad74@gmail.com>
2022-12-22 01:26:42 +0200

    change let and fix cancel bug

--- withMath.js
+++ withMath.js
@@ -1,12 +1,15 @@
-let input1  = prompt("ведить довжину катетів трикутника через пробіл")
+const input1  = prompt("ведить довжину катетів трикутника через пробіл")
+if(input !== null) {
+    const res1 = input1.split(' ')
+    const first1 = Number(res1[0] )
+    const second1 = Number(res1[1])
 
-let res1 = (input1.split(' '))
-let first1 = res[0]*1
-let second1 = res[1]*1
-
-if(!isNaN(first1) && !isNaN(second1)&& res1.length === 2 && first1> 0 && second1 >0) {
-    let hypotenuse = Math.sqrt((first1**2)+(second1**2))
-    alert(`Гіпотенуза: ${hypotenuse.toFixed(2)}`)
+    if (!isNaN(first1) && !isNaN(second1) && res1.length === 2 && first1 > 0 && second1 > 0) {
+        const hypotenuse = Math.sqrt((first1 ** 2) + (second1 ** 2)).toFixed((2))
+        alert(`Гіпотенуза: ${hypotenuse}`)
+    } else {
+        alert('Данні нажаль не вірні. Вам потрібно впісати два числа через пробіл і більше 0')
+    }
 }else {
-    alert('Данні нажаль не вірні. Вам потрібно впісати два числа через пробіл і більше 0')
+    alert('Ви не ввели данні ')
 }
